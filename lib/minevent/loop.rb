e4070ee229e1cf18a50fca01f515a480b1ca39d5
Matthew Sadler <mat@sourcetagsandcodes.com>
2010-04-11 14:39:20 +0100

    Don't remove connections from the loop's collection till we are done with all the callbacks

--- lib/minevent/loop.rb
+++ lib/minevent/loop.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module Minevent
   module Loop
     class << self
@@ -12,8 +14,7 @@ module Minevent
       end
       
       def remove(connection)
-        @real_to_wrap_cache.delete(connection.real) if @real_to_wrap_cache
-        connections.delete(connection)
+        garbage.add(connection)
       end
       
       def run(timeout=0.25)
@@ -28,6 +29,7 @@ module Minevent
           readable.each {|r| wrapper(r).notify_readable} if readable
           writeable.each {|w| wrapper(w).notify_writeable} if writeable
           errored.each {|e| wrapper(e).notify_errored} if errored
+          collect_garbage
         end
       end
       
@@ -36,6 +38,17 @@ module Minevent
         (@real_to_wrap_cache ||= Hash.new do |hash, key|
           hash[key] = connections.find {|connect| connect.real == key}
         end)[connection]
+      end
+      
+      def garbage
+        @garbage ||= Set.new
+      end
+      
+      def collect_garbage
+        garbage.each do |connection|
+          @real_to_wrap_cache.delete(connection.real) if @real_to_wrap_cache
+          connections.delete(connection)
+        end.clear
       end
       
     end
